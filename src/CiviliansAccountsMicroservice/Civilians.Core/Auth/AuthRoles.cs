0935572c74bea2140971755a054c014552850ccf
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-04-05 19:30:50 +0300

    Added auth policies and roles

---
+++ src/CiviliansAccountsMicroservice/Civilians.Core/Auth/AuthRoles.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Civilians.Core.Auth
+{
+    public class AuthRoles
+    {
+        public const string Civilian = "Civilian";
+        public const string Administrator = "Administrator";
+
+        public static List<IdentityRole<Guid>> Roles { get; } = new()
+        {
+            new IdentityRole<Guid>(Civilian) { Id = Guid.NewGuid(), NormalizedName = Civilian.Normalize() },
+            new IdentityRole<Guid>(Administrator) { Id = Guid.NewGuid(), NormalizedName = Administrator.Normalize() }
+        };
+    }
+}
