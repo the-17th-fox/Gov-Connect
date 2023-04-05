80dadfbefb2ba043483e91ab5b1f02a2c404ec11
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-04-05 19:30:30 +0300

    Added db models

---
+++ src/CiviliansAccountsMicroservice/Civilians.Core/Models/Passport.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Civilians.Core.Models
+{
+    [Index(nameof(FirstName), nameof(LastName), nameof(Patronymic))]
+    public class Passport
+    {
+        public Guid Id { get; set; }
+
+        public Civilian Civilian { get; set; } = null!;
+
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+        [Required]
+        public string Patronymic { get; set; } = string.Empty;
+
+        // Passport number
+        [Required]
+        public string Region { get; set; } = string.Empty;
+        [Required]
+        public string Number { get; set; } = string.Empty;
+    }
+}
---
+++ src/CiviliansAccountsMicroservice/Civilians.Core/Models/RefreshToken.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Civilians.Core.Models
+{
+    [Index(nameof(CivilianId), IsUnique = true)]
+    public class RefreshToken
+    {
+        [Key]
+        public Guid Token { get; set; }
+        public DateTime ExpiresAt { get; set; }
+        public DateTime IssuedAt { get; set; } = DateTime.UtcNow;
+        public bool IsRevoked { get; set; } = false;
+
+        public Guid CivilianId { get; set; }
+        public Civilian Civilian { get; set; } = null!;
+
+        [NotMapped]
+        public bool IsExpired => DateTime.UtcNow >= ExpiresAt;
+        [NotMapped]
+        public bool IsActive => !(IsExpired || IsRevoked);
+
+        public RefreshToken(DateTime expiresAt, Guid accountId)
+        {
+            ExpiresAt = expiresAt;
+            CivilianId = accountId;
+        }
+    }
+}
