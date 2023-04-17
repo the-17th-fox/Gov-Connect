ed64b0dd7b1064de2f37fa5af9ccde245bb6fa7f
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-04-17 18:49:50 +0300

    Added tokens controllers

---
+++ src/CiviliansAccountsMicroservice/Civilians.Api/Controllers/Tokens/AdminTokensController.cs
@@ -0,0 +1,37 @@
+﻿using Civilians.Application.Interfaces;
+using Civilians.Core.Auth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Civilians.Api.Controllers.Tokens
+{
+    [Authorize(Policy = AuthPolicies.Administrators)]
+    [Route("api/tokens")]
+    [ApiController]
+    public class AdminTokensController : ControllerBase
+    {
+        private readonly ITokensService _tokensService;
+
+        public AdminTokensController(ITokensService tokensService)
+        {
+            _tokensService = tokensService ?? throw new ArgumentNullException(nameof(tokensService));
+        }
+
+        [HttpPatch("revoke/{userId}")]
+        public async Task<IActionResult> RevokeRefreshTokenAsync(Guid userId)
+        {
+            await _tokensService.RevokeRefreshTokenAsync(userId);
+            
+            return Ok();
+        }
+
+        [HttpPatch("revoke")]
+        public async Task<IActionResult> RevokeAllRefreshTokensAsync()
+        {
+            await _tokensService.RevokeAllRefreshTokensAsync();
+            
+            return Ok();
+        }
+    }
+}
