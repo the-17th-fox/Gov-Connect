a37d2088eaa7feaed29918824760d7fe7a209c56
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-04-18 21:54:04 +0300

    Added users controllers

---
+++ src/AuthoritiesAccountsMicroservice/Authorities.Api/ViewModels/Pagination/PageViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Authorities.Api.ViewModels.Pagination
+{
+    public class PageViewModel<T>
+    {
+        public List<T> Items { get; set; } = new();
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public bool HasNextPage { get; set; }
+        public bool HasPreviousPage { get; set; }
+    }
+}
