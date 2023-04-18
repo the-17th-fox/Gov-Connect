9c9db8d35401feefcb5ec16919ce33680577ace5
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-04-18 15:02:37 +0300

    Added services interfaces

---
+++ src/AuthoritiesAccountsMicroservice/Authorities.Application/Interfaces/IUsersService.cs
@@ -0,0 +1,24 @@
+﻿using Authorities.Application.ViewModels.Pagination;
+using Authorities.Application.ViewModels.Tokens;
+using Authorities.Application.ViewModels.Users;
+using Authorities.Core.Models;
+
+namespace Authorities.Application.Interfaces
+{
+    public interface IUsersService
+    {
+        // Auth methods
+        public Task CreateAsync(RegistrationViewModel registrationsParams);
+        public Task<TokensViewModel> LoginAsync(LoginViewModel loginViewModel);
+
+        // Management methods
+        public Task BlockAsync(Guid id);
+        public Task UnblockAsync(Guid id);
+        public Task<User> GetByIdAsync(Guid id);
+        public Task<List<User>> GetAllAsync(UsersPaginationParametersViewModel pageParams);
+        public Task<IList<string>> GetRolesAsync(User user);
+        public Task ChangeRolesAsync(Guid id, string roleName);
+        public Task RemoveRolesAsync(Guid id);
+        public Task<List<User>> GetNotConfirmedAsync(PaginationParametersViewModel pageParams);
+    }
+}
