ae3e01c8ef350443d0ad3d9f0f96d892790431f1
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-04-19 20:23:18 +0300

    Added AlreadyExists and BadRequest custom exceptions

---
+++ src/CommunicationsMicroservice/Communications.Core/CustomExceptions/AlreadyExistsException.cs
@@ -0,0 +1,6 @@
+﻿namespace Communications.Core.CustomExceptions;
+
+public class AlreadyExistsException : Exception
+{
+    public AlreadyExistsException(string message = "An object with such parameters is already present in the database.") : base(message) {}
+}
---
+++ src/CommunicationsMicroservice/Communications.Core/CustomExceptions/BadRequestException.cs
@@ -0,0 +1,6 @@
+﻿namespace Communications.Core.CustomExceptions;
+
+public class BadRequestException : Exception
+{
+    public BadRequestException(string message) : base(message) {}
+}
