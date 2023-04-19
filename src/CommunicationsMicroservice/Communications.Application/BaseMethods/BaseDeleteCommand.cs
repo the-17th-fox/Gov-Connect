a33af6631a741a4261b4cc2209b02a69c13c75f9
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-04-19 20:24:14 +0300

    Removed redunant classes

--- src/CommunicationsMicroservice/Communications.Application/BaseMethods/BaseDeleteCommand.cs
+++
@@ -1,8 +0,0 @@
-﻿using MediatR;
-
-namespace Communications.Application.BaseMethods;
-
-public class BaseDeleteCommand : IRequest
-{
-    public Guid Id { get; set; }
-}
--- src/CommunicationsMicroservice/Communications.Application/BaseMethods/BaseMessageManagingCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace Communications.Application.BaseMethods;
-
-public class BaseMessageCreateCommand : IRequest
-{
-    public string Header { get; set; } = string.Empty;
-    public string Body { get; set; } = string.Empty;
-}
