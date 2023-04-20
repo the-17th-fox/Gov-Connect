dfbafb33c6881680aad59e4c369a5c4cca81e96f
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-04-20 17:44:32 +0300

    Added db context and entities configurations

---
+++ src/CommunicationsMicroservice/Communications.Infrastructure/DbContext/Configurations/RepliesConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Communications.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Communications.Infrastructure.DbContext.Configurations;
+
+public class RepliesConfiguration : IEntityTypeConfiguration<Reply>
+{
+    public void Configure(EntityTypeBuilder<Reply> builder)
+    {
+        builder.Property(r => r.CreatedAt)
+            .HasDefaultValueSql("GETUTCDATE()")
+            .ValueGeneratedOnAdd();
+
+        builder.Property<DateTime>("UpdatedAt")
+            .HasDefaultValueSql("GETUTCDATE()")
+            .ValueGeneratedOnAddOrUpdate();
+    }
+}
