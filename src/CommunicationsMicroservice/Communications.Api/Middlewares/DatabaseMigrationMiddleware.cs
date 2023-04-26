5db9f41abffe7bcc2cda1512f290b4e25be3409d
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-04-26 19:37:40 +0300

    Added db migration middleware

---
+++ src/CommunicationsMicroservice/Communications.Api/Middlewares/DatabaseMigrationMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using Communications.Infrastructure.DbContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace Communications.Api.Middlewares;
+
+public static class DatabaseMigrationMiddleware
+{
+    public static async Task<WebApplication> MigrateDatabase(this WebApplication app)
+    {
+        await using var scope = app.Services.CreateAsyncScope();
+        await using var context = scope.ServiceProvider.GetRequiredService<CommunicationsDbContext>();
+
+        if ((await context.Database.GetPendingMigrationsAsync()).Any())
+        {
+            await context.Database.EnsureCreatedAsync();
+            await context.Database.MigrateAsync();
+        }
+
+        return app;
+    }
+}
