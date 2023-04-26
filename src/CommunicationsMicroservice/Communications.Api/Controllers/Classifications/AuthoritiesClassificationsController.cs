1a58c1ee99a4be8453277b8c109dd50b0f7739f3
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-04-26 19:34:21 +0300

    Added Classifications controllers

---
+++ src/CommunicationsMicroservice/Communications.Api/Controllers/Classifications/AuthoritiesClassificationsController.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using Communications.Api.ViewModels.Classifications;
+using Communications.Application.Classifications.Commands;
+using Communications.Application.Classifications.Commands.DeleteClassification;
+using Communications.Application.Classifications.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Communications.Api.Controllers.Classifications
+{
+    [Route("api/classifications")]
+    [ApiController]
+    public class AuthoritiesClassificationsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+
+        public AuthoritiesClassificationsController(IMediator mediator, IMapper mapper)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(Guid id)
+        {
+            var classification = await _mediator.Send(new GetClassificationByIdQuery() { Id = id });
+            var classificationViewModel = _mapper.Map<ClassificationViewModel>(classification);
+
+            return Ok(classificationViewModel);
+        }
+
+        [HttpPost("new")]
+        public async Task<IActionResult> CreateAsync(CreateClassificationCommand createClassificationCommand)
+        {
+            await _mediator.Send(createClassificationCommand);
+
+            return Created("new", null);
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> UpdateAsync(Guid id, [FromBody] string newName)
+        {
+            await _mediator.Send(new UpdateClassificationCommand() { Id = id, NewName = newName });
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(Guid id)
+        {
+            await _mediator.Send(new DeleteClassificationCommand() { Id = id });
+
+            return Ok();
+        }
+    }
+}
---
+++ src/CommunicationsMicroservice/Communications.Api/Controllers/Classifications/AuthorizedClassificationsController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Communications.Api.ViewModels.Classifications;
+using Communications.Api.ViewModels.Pagination;
+using Communications.Application.Classifications.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Communications.Api.Controllers.Classifications;
+
+[Route("api/classifications")]
+[ApiController]
+public class AuthorizedClassificationsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    private readonly IMapper _mapper;
+
+    public AuthorizedClassificationsController(IMediator mediator, IMapper mapper)
+    {
+        _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> GetAllAsync(GetAllClassificationsQuery getAllClassificationsQuery)
+    {
+        var classifications = await _mediator.Send(getAllClassificationsQuery);
+        var classificationsViewModel = _mapper.Map<PageViewModel<ClassificationViewModel>>(classifications);
+
+        return Ok(classificationsViewModel);
+    }
+}
