6af4176dbb6baa262130f66a397c49721d7760d5
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-04-26 19:35:49 +0300

    Added Notifications controller

---
+++ src/CommunicationsMicroservice/Communications.Api/ViewModels/Notifications/NotificationViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Communications.Api.ViewModels.Classifications;
+
+namespace Communications.Api.ViewModels.Notifications;
+
+public class NotificationViewModel
+{
+    public Guid Id { get; set; }
+    public List<ClassificationViewModel> Classifications { get; set; } = new();
+    public string Organization { get; set; } = string.Empty;
+    public string Header { get; set; } = string.Empty;
+    public string Body { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
---
+++ src/CommunicationsMicroservice/Communications.Api/ViewModels/Notifications/ShortNotificationViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Communications.Api.ViewModels.Classifications;
+
+namespace Communications.Api.ViewModels.Notifications;
+
+public class ShortNotificationViewModel
+{
+    public Guid Id { get; set; }
+    public List<ClassificationViewModel> Classifications { get; set; } = new();
+    public string Organization { get; set; } = string.Empty;
+    public string Header { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
