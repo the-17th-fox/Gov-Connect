f8b47a9960bc84a1c07ef0c34c03417a0118e82c
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-04-26 19:32:48 +0300

    Added GetReplyByReport command and handler

---
+++ src/CommunicationsMicroservice/Communications.Application/Replies/Queries/GetReplyByReport/GetReplyByReportQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using Communications.Core.CustomExceptions;
+using Communications.Core.Interfaces;
+using Communications.Core.Models;
+using MediatR;
+
+namespace Communications.Application.Replies.Queries.GetReplyByReport;
+
+public class GetReplyByReportQueryHandler : RepliesHandlerBase, IRequestHandler<GetReplyByReportQuery, Reply>
+{
+    public GetReplyByReportQueryHandler(IUnitOfWork unitOfWork) : base(unitOfWork)
+    {
+    }
+
+    public async Task<Reply> Handle(GetReplyByReportQuery request, CancellationToken cancellationToken)
+    {
+        var report = await UnitOfWork.RepliesRepository.GetForReportAsync(request.ReportId);
+        if (report == null)
+        {
+            throw new NotFoundException();
+        }
+
+        return report;
+    }
+}
