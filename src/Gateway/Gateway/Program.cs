a887ae6304a06d5d552e20b7a32b95d802b580b3
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-04-30 19:25:29 +0300

    Refactored gateway configs

--- src/Gateway/Gateway/Program.cs
+++ src/Gateway/Gateway/Program.cs
@@ -1,31 +1,12 @@
-using Gateway.Authentication;
-using Ocelot.DependencyInjection;
+using Gateway.Configuration;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 var configuration = builder.Configuration;
 
-configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
-configuration.AddJsonFile("docker-configuration.json", optional: true, reloadOnChange: true);
-services.AddOcelot(configuration);
-
-services.AddAuthentication(options =>
-{
-    options.DefaultScheme = GovConnectAuthOptions.Scheme;
-})
-    .AddCustomAuthentication(GovConnectAuthOptions.Scheme, opt =>
-    {
-        opt.TokenValidationParameters = new()
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true
-        };
-
-        opt.ConfigurationSection = configuration.GetSection("Authentication").GetSection("Jwt");
-    });
+services.ConfigureAuthentication(configuration);
+services.ConfigureOcelot(configuration);
 
 var app = builder.Build();
 
