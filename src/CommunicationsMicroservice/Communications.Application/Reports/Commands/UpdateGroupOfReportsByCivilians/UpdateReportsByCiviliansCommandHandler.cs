3660a947fddaba3250b66962a072e9b5f9f4c6c9
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-05-02 21:47:04 +0300

    Added CQRS command for updating set of reports by set of civiliansids

---
+++ src/CommunicationsMicroservice/Communications.Application/Reports/Commands/UpdateGroupOfReportsByCivilians/UpdateReportsByCiviliansCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using Communications.Core.Interfaces;
+using MediatR;
+
+namespace Communications.Application.Reports.Commands;
+
+public class UpdateReportsByCiviliansCommandHandler : ReportsHandlerBase, IRequestHandler<UpdateReportsByCiviliansCommand>
+{
+    public UpdateReportsByCiviliansCommandHandler(IUnitOfWork unitOfWork) : base(unitOfWork)
+    {
+    }
+
+    public async Task Handle(UpdateReportsByCiviliansCommand request, CancellationToken cancellationToken)
+    {
+        var civiliansIds = request.CiviliansData.Select(cd => cd.CivilianId);
+        var reports = await UnitOfWork.ReportsRepository.GetAllByCiviliansAsync(civiliansIds);
+
+        foreach (var report in reports)
+        {
+            var civilianData = request.CiviliansData.FirstOrDefault(cd => cd.CivilianId == report.CivilianId);
+
+            if (civilianData != null)
+            {
+                UnitOfWork.ReportsRepository.Update(report);
+                report.FirstName = civilianData.FirstName;
+                report.Patronymic = civilianData.Patronymic;
+            }
+        }
+
+        await UnitOfWork.SaveChangesAsync();
+    }
+}
