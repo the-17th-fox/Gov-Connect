688359428ea140b0f91fd94ad8e02315a270d7e0
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-05-09 19:58:14 +0300

    Added kafka consumer configuration

--- src/CommunicationsMicroservice/Communications.Hangfire/Services/CiviliansInfoConsistencyService.cs
+++ src/CommunicationsMicroservice/Communications.Hangfire/Services/CiviliansInfoConsistencyService.cs
@@ -1,42 +1,59 @@
-﻿using AutoMapper;
-using Communications.Application.AutoMapper.CiviliansInfoConsistency;
+﻿using Communications.Application.AutoMapper.CiviliansInfoConsistency;
 using Communications.Application.Reports.Commands;
 using Communications.Hangfire.Interfaces;
 using MediatR;
+using SharedLib.Kafka.Interfaces;
 
 namespace Communications.Hangfire.Services;
 
 public class CiviliansInfoConsistencyService : ICiviliansInfoConsistencyService
 {
     private readonly IMediator _mediator;
-    private readonly IMapper _mapper;
+    private readonly IKafkaConsumer<CivilianInfoViewModel> _consumer;
 
     public const string RecurringJobId = "civilians-info-in-reports-update";
 
-    public CiviliansInfoConsistencyService(IMediator mediator, IMapper mapper)
+    public CiviliansInfoConsistencyService(IMediator mediator, IKafkaConsumer<CivilianInfoViewModel> consumer)
     {
         _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
-        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        _consumer = consumer ?? throw new ArgumentNullException(nameof(consumer));
     }
 
     public async Task UpdateCiviliansInfoAsync(CancellationToken cancellationToken)
     {
-        var civiliansDataViewModel = await GetUpdatedCiviliansDataAsync(cancellationToken);
+        var civiliansDataViewModel = ReadUpdatedCiviliansInfo();
 
         var updateCommand = new UpdateReportsByCiviliansCommand()
         {
             CiviliansData = civiliansDataViewModel
         };
 
-        await _mediator.Send(updateCommand, cancellationToken);
+        if (civiliansDataViewModel.Any())
+        {
+            await _mediator.Send(updateCommand, cancellationToken);
+        }
 
         await Task.CompletedTask;
     }
 
-    private async Task<List<CivilianInfoViewModel>> GetUpdatedCiviliansDataAsync(CancellationToken stoppingToken)
+    private List<CivilianInfoViewModel> ReadUpdatedCiviliansInfo()
     {
-        // Add taking new messages from kafka here,
-        // possibly another lib for kafka
-        throw new NotImplementedException();
+        var civiliansInfo = new List<CivilianInfoViewModel>();
+        var continueReading = true;
+
+        while (continueReading)
+        {
+            _consumer.Consume(out var civilianInfo);
+
+            if (civilianInfo == null)
+            {
+                continueReading = false;
+                continue;
+            }
+
+            civiliansInfo.Add(civilianInfo);
+        }
+
+        return civiliansInfo;
     }
 }
