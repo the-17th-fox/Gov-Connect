6e58ee5782a78b0675a6175c43c149553e3979ba
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-05-09 19:57:19 +0300

    Fixed service creation in HangfireHostedService

--- src/CommunicationsMicroservice/Communications.Hangfire/HangfireHostedService.cs
+++ src/CommunicationsMicroservice/Communications.Hangfire/HangfireHostedService.cs
@@ -1,6 +1,7 @@
 ﻿using Communications.Hangfire.Interfaces;
 using Communications.Hangfire.Services;
 using Hangfire;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace Communications.Hangfire;
@@ -8,22 +9,23 @@ namespace Communications.Hangfire;
 public class HangfireHostedService : BackgroundService
 {
     private readonly IRecurringJobManager _recurringJobManager;
-    private readonly ICiviliansInfoConsistencyService _civiliansInfoConsistencyService;
+    private readonly IServiceScopeFactory _scopeFactory;
 
-    public HangfireHostedService(IRecurringJobManager recurringJobManager, ICiviliansInfoConsistencyService civiliansInfoConsistencyService)
+    public HangfireHostedService(IRecurringJobManager recurringJobManager, IServiceScopeFactory scopeFactory)
     {
         _recurringJobManager = recurringJobManager ?? throw new ArgumentNullException(nameof(recurringJobManager));
-
-        _civiliansInfoConsistencyService = civiliansInfoConsistencyService ??
-                                           throw new ArgumentNullException(nameof(civiliansInfoConsistencyService));
+        _scopeFactory = scopeFactory ?? throw new ArgumentNullException(nameof(scopeFactory));
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        using var scope = _scopeFactory.CreateScope();
+        var civiliansInfoConsistencyService = scope.ServiceProvider.GetRequiredService<ICiviliansInfoConsistencyService>();
+
         _recurringJobManager.AddOrUpdate(
             CiviliansInfoConsistencyService.RecurringJobId,
-            () => _civiliansInfoConsistencyService.UpdateCiviliansInfoAsync(stoppingToken),
-            Cron.Hourly());
+            () => civiliansInfoConsistencyService.UpdateCiviliansInfoAsync(stoppingToken),
+            Cron.Hourly);
 
         await Task.CompletedTask;
     }
