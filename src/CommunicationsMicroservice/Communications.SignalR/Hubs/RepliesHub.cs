c81f68ae3c209d625ba27572d6506e1487cea645
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-05-11 23:34:14 +0300

    Added SignalR lib project with RepliesHub

---
+++ src/CommunicationsMicroservice/Communications.SignalR/Hubs/RepliesHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Communications.SignalR.Hubs;
+
+public class RepliesHub : Hub
+{
+    public const string NewReplyMethodName = "NewReply";
+
+    public async Task JoinReportGroupAsync(string reportId)
+    {
+        if (string.IsNullOrEmpty(reportId))
+        {
+            throw new ArgumentNullException(nameof(reportId));
+        }
+
+        await Groups.AddToGroupAsync(Context.ConnectionId, reportId);
+    }
+
+    public async Task LeaveReportGroupAsync(string reportId)
+    {
+        if (string.IsNullOrEmpty(reportId))
+        {
+            throw new ArgumentNullException(nameof(reportId));
+        }
+
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, reportId);
+    }
+}
