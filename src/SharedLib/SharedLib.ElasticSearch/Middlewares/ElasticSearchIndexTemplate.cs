24eec9b4fda114752fa3b3a8cbc4f23f36cd8bda
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-05-21 23:53:30 +0300

    Added elastic search project into shared lib

---
+++ src/SharedLib/SharedLib.ElasticSearch/Middlewares/ElasticSearchIndexTemplate.cs
@@ -0,0 +1,13 @@
+﻿namespace SharedLib.ElasticSearch.Middlewares;
+
+public class ElasticSearchIndexTemplate
+{
+    public string IndexName { get; set; }
+    public Type MessageType { get; set; }
+
+    public ElasticSearchIndexTemplate(string indexName, Type messageType)
+    {
+        IndexName = indexName;
+        MessageType = messageType;
+    }
+}
