a652d076528bf343486eeb2a2ae2c82623a971ec
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-05-21 23:57:55 +0300

    Added search extensions

---
+++ src/CommunicationsMicroservice/Communications.Application/Utilities/ElasticSearchIndexesOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Communications.Application.Utilities;
+
+public class ElasticSearchIndexesOptions
+{
+    public string ReportsIndexName { get; set; } = string.Empty;
+    public string NotificationsIndexName { get; set; } = string.Empty;
+    public byte QueriedElementsAmount { get; set; }
+}
