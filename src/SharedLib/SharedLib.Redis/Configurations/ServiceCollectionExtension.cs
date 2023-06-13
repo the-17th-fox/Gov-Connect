72b3a677da4a7d9905c1f105b26f43e705ad5669
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-06-14 00:55:11 +0300

    Added ServiceCollection extension

---
+++ src/SharedLib/SharedLib.Redis/Configurations/ServiceCollectionExtension.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SharedLib.Redis.Interfaces;
+using StackExchange.Redis;
+
+namespace SharedLib.Redis.Configurations;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddRedis(this IServiceCollection services, string connectionString)
+    {
+        services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(connectionString));
+
+        services.AddScoped<IRedisService, IRedisService>();
+        
+        return services;
+    }
+
+    public static IServiceCollection AddRedisConfiguration(this IServiceCollection services, 
+        Action<RedisCacheConfiguration> cacheConfiguration)
+    {
+        services.Configure(cacheConfiguration);
+
+        return services;
+    }
+}
