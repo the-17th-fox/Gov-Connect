1939b45142152f60eae10b17c4db0afeb7fad925
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-06-14 00:54:27 +0300

    Added RedisService

---
+++ src/SharedLib/SharedLib.Redis/Implementation/RedisService.cs
@@ -0,0 +1,75 @@
+﻿using System.Globalization;
+using SharedLib.Redis.Interfaces;
+using StackExchange.Redis;
+
+namespace SharedLib.Redis.Implementation;
+
+public class RedisService : IRedisService
+{
+    private readonly IDatabase _redis;
+
+    public RedisService(IConnectionMultiplexer multiplexer)
+    {
+        CheckMultiplexerConnection(multiplexer);
+        
+        _redis = multiplexer.GetDatabase();
+    }
+
+    public async Task<string> TryGetFromCacheAsync(string cacheKey)
+    {
+        CheckStorageConnection(cacheKey);
+
+        string? response = await _redis.StringGetAsync(cacheKey);
+
+        return response ?? string.Empty;
+    }
+
+    public async Task<TaskStatus> SetCacheAsync<TValue>(string cacheKey, TValue cacheValue, short timeToLiveSeconds)
+    {
+        CheckStorageConnection(cacheKey);
+
+        var parsedValue = Convert.ToString(cacheValue, CultureInfo.InvariantCulture);
+
+        return await SetCacheAsync(cacheKey, parsedValue!, timeToLiveSeconds);
+    }
+
+    public async Task<TaskStatus> SetCacheAsync(string cacheKey, string cacheValue, short timeToLiveSeconds)
+    {
+        if (string.IsNullOrWhiteSpace(cacheValue))
+        {
+            throw new ArgumentNullException(nameof(cacheValue));
+        }
+
+        CheckStorageConnection(cacheKey);
+
+        var transaction = _redis.CreateTransaction(new());
+
+        _ = transaction.StringSetAsync(cacheKey, cacheValue);
+        _ = transaction.KeyExpireAsync(cacheKey, DateTime.UtcNow.AddMinutes(timeToLiveSeconds));
+
+        await transaction.ExecuteAsync();
+
+        return TaskStatus.RanToCompletion;
+    }
+
+    private void CheckMultiplexerConnection(IConnectionMultiplexer connectionMultiplexer)
+    {
+        if (connectionMultiplexer is null)
+        {
+            throw new ArgumentNullException(nameof(connectionMultiplexer));
+        }
+
+        if (!connectionMultiplexer.IsConnected)
+        {
+            throw new RedisConnectionException(ConnectionFailureType.UnableToConnect, string.Empty);
+        }
+    }
+
+    private void CheckStorageConnection(string key)
+    {
+        if (!_redis.IsConnected(key))
+        {
+            throw new RedisConnectionException(ConnectionFailureType.ConnectionDisposed, $"Key:[{key}]");
+        }
+    }
+}
---
+++ src/SharedLib/SharedLib.Redis/Interfaces/IRedisService.cs
@@ -0,0 +1,8 @@
+﻿namespace SharedLib.Redis.Interfaces;
+
+public interface IRedisService
+{
+    public Task<string> TryGetFromCacheAsync(string cacheKey);
+    public Task<TaskStatus> SetCacheAsync<TValue>(string cacheKey, TValue cacheValue, short timeToLiveSeconds);
+    public Task<TaskStatus> SetCacheAsync(string cacheKey, string cacheValue, short timeToLiveSeconds);
+}
