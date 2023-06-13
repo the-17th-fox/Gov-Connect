799702fc38faec22d695ea3777a13d47fdeb3cba
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-06-14 00:54:00 +0300

    Added Redis lib and cache configuration

---
+++ src/SharedLib/SharedLib.Redis/Configurations/RedisCacheConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace SharedLib.Redis.Configurations;
+
+public class RedisCacheConfiguration
+{
+    /// <summary>
+    /// Uses if another expiration span hasn't been provided
+    /// </summary>
+    public short TimeToLiveSeconds { get; set; }
+    public bool IsEnabled { get; set; }
+}
